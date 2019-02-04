6c8e33cd804f13df6eee5d78aa91076c86e774ca
RicoBSJ <aubrun.eric@free.fr>
2019-02-04 22:48:42 +0100

    5 Commit Home 04_02

--- src/Test.java
+++ src/Test.java
@@ -1,48 +1,35 @@
 
-//Package à importer afin d'utiliser l'objet File
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Test {
 	public static void main(String[] args) {
-		// Création de l'objet File
-		File f = new File("test.txt");
-		System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
-		System.out.println("Nom du fichier : " + f.getName());
-		System.out.println("Est-ce qu'il existe ? " + f.exists());
-		System.out.println("Est-ce un répertoire ? " + f.isDirectory());
-		System.out.println("Est-ce un fichier ? " + f.isFile());
+		// Crï¿½ation de l'objet File
+		Path path = Paths.get("test.txt");
+		System.out.println("Chemin absolu du fichier : " + path.toAbsolutePath());
+		System.out.println("Est-ce qu'il existe ? " + Files.exists(path));
+		System.out.println("Nom du fichier : " + path.getFileName());
+		System.out.println("Est-ce un rÃ©pertoire ? " + Files.isDirectory(path));
 
-		System.out.println("Affichage des lecteurs à la racine du PC : ");
-		// On récupère maintenant la liste des répertoires dans une collection typée
-		// Via l'objet FileSystem qui représente le système de fichier de l'OS
-		// hébergeant la JVM
-		Iterable<Path> roots = FileSystems.getDefault().getRootDirectories();
+		// On rÃ©cupÃ¨re maintenant la liste des rÃ©pertoires dans une collection typÃ©e
+		// Via l'objet FileSystem qui reprÃ©sente le systÃ¨me de fichier de l'OS
+		// hÃ©bergeant la JVM
+		// Iterable<Path> roots = FileSystems.getDefault().getRootDirectories();
 
-		// Maintenant, il ne nous reste plus qu'à parcourir
-		for (Path chemin : roots) {
-			System.out.println(chemin);
-			// Pour lister un répertoire, il faut utiliser l'objet DirectoryStream
-			// L'objet Files permet de créer ce type d'objet afin de pouvoir l'utiliser
-			try(DirectoryStream<Path> listing = Files.newDirectoryStream(chemin, "*.txt"))
-			//Ne prendra en compte que les fichier ayant l'extension .txt
-			{
-
-				int i = 0;
-				for (Path nom : listing) {
-					System.out.print("\t\t" + ((Files.isDirectory(nom)) ? nom + "/" : nom));
-					i++;
-					if (i % 4 == 0)
-						System.out.println("\n");
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		// Maintenant, il ne nous reste plus qu'Ã  parcourir
+		// for (Path chemin : roots) {
+		// System.out.println(chemin);
+		// Pour lister un rÃ©pertoire, il faut utiliser l'objet DirectoryStream
+		// L'objet Files permet de crÃ©er ce type d'objet afin de pouvoir l'utiliser
+		Path source = Paths.get("test2.txt");
+		Path cible = Paths.get("test3.txt");
+		try {
+			Files.move(source, cible, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
