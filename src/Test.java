d654e3efa26b9a2c7b2f16813157a64edc0fd228
aubrune <aubrune@P520.ari.asso.fr>
2019-02-04 18:03:15 +0100

    4 Commit Job 04_02

--- src/Test.java
+++ src/Test.java
@@ -1,40 +1,48 @@
+
+//Package à importer afin d'utiliser l'objet File
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Test {
-  public static void main(String[] args) {		
-    File file = new File("testFileWriter.txt");
-    FileWriter fw;
-    FileReader fr;
-		
-    try {
-      //Création de l'objet
-      fw = new FileWriter(file);
-      String str = "Bonjour à tous, amis Zéros !\n";
-      str += "\tComment allez-vous ? \n";
-      //On écrit la chaîne
-      fw.write(str);
-      //On ferme le flux
-      fw.close();
-			
-      //Création de l'objet de lecture
-      fr = new FileReader(file);
-      str = "";
-      int i = 0;
-      //Lecture des données
-      while((i = fr.read()) != -1)
-        str += (char)i;
+	public static void main(String[] args) {
+		// Création de l'objet File
+		File f = new File("test.txt");
+		System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
+		System.out.println("Nom du fichier : " + f.getName());
+		System.out.println("Est-ce qu'il existe ? " + f.exists());
+		System.out.println("Est-ce un répertoire ? " + f.isDirectory());
+		System.out.println("Est-ce un fichier ? " + f.isFile());
 
-      //Affichage
-      System.out.println(str);
+		System.out.println("Affichage des lecteurs à la racine du PC : ");
+		// On récupère maintenant la liste des répertoires dans une collection typée
+		// Via l'objet FileSystem qui représente le système de fichier de l'OS
+		// hébergeant la JVM
+		Iterable<Path> roots = FileSystems.getDefault().getRootDirectories();
 
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+		// Maintenant, il ne nous reste plus qu'à parcourir
+		for (Path chemin : roots) {
+			System.out.println(chemin);
+			// Pour lister un répertoire, il faut utiliser l'objet DirectoryStream
+			// L'objet Files permet de créer ce type d'objet afin de pouvoir l'utiliser
+			try(DirectoryStream<Path> listing = Files.newDirectoryStream(chemin, "*.txt"))
+			//Ne prendra en compte que les fichier ayant l'extension .txt
+			{
+
+				int i = 0;
+				for (Path nom : listing) {
+					System.out.print("\t\t" + ((Files.isDirectory(nom)) ? nom + "/" : nom));
+					i++;
+					if (i % 4 == 0)
+						System.out.println("\n");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
