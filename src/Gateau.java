f72b484f2a0e289a8dd60f30a864918d8508a8ac
RicoBSJ <aubrun.eric@free.fr>
2019-02-05 23:54:35 +0100

    1 Commit Home 05_02

---
+++ src/Gateau.java
@@ -0,0 +1,7 @@
+
+public class Gateau extends Patisserie {
+
+	public String preparer() {
+		return "Je suis un gâteau et je suis constitué des éléments suivants. \n";
+	}
+}
---
+++ src/Main_Patisserie.java
@@ -0,0 +1,8 @@
+
+public class Main_Patisserie {
+
+	public static void main(String[] args) {
+		Patisserie pat = new CoucheChocolat(new CoucheCaramel(new CoucheBiscuit(new CoucheChocolat(new Gateau()))));
+		System.out.println(pat.preparer());
+	}
+}
