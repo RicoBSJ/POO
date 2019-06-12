69793a19175a728bfae2b6dfe79c4e3c1ee7c731
RicoBSJ <aubrun.eric@free.fr>
2019-06-12 23:42:19 +0200

    4 Commit Home 12_06

--- src/io/WriteFile1.java
+++ src/io/WriteFile1.java
@@ -1,39 +1,43 @@
 package io;
 
 //Package à importer afin d'utiliser l'objet File
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
 
 public class WriteFile1 {
-public static void main(String[] args) {
-  //Création de l'objet File
-  File f = new File("test.txt");
-  System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
-  System.out.println("Nom du fichier : " + f.getName());
-  System.out.println("Est-ce qu'il existe ? " + f.exists());
-  System.out.println("Est-ce un répertoire ? " + f.isDirectory());
-  System.out.println("Est-ce un fichier ? " + f.isFile());
-	
-  System.out.println("Affichage des lecteurs à la racine du PC : ");
-  for(File file : File.listRoots())
-  {
-    System.out.println(file.getAbsolutePath());
-    try {
-      int i = 1;	
-      //On parcourt la liste des fichiers et répertoires
-      for(File nom : file.listFiles()){
-        //S'il s'agit d'un dossier, on ajoute un "/"
-        System.out.print("\t\t" + ((nom.isDirectory()) ? nom.getName()+"/" : nom.getName()));
-					
-        if((i%4) == 0){
-          System.out.print("\n");
-        }
-        i++;
-      }
-      System.out.println("\n");
-    } catch (NullPointerException e) {
-      //L'instruction peut générer une NullPointerException
-      //s'il n'y a pas de sous-fichier !
-    }
-  }		
-}
+	public static void main(String[] args) {
+		// Création de l'objet File
+		Path path = Paths.get("test.txt");
+		System.out.println("Chemin absolu du fichier : " + path.toAbsolutePath());
+		System.out.println("Est-ce qu'il existe ? " + Files.exists(path));
+		System.out.println("Nom du fichier : " + path.getFileName());
+		System.out.println("Est-ce un répertoire ? " + Files.isDirectory(path));
+		System.out.println("Est-ce un fichier ? " + Files.isRegularFile(path));
+
+		System.out.println("Affichage des lecteurs à la racine du PC : ");
+		// On récupère maintenant la liste des répertoires dans une collection typée
+		// Via l'objet FileSystem qui représente le système de fichier de l'OS
+		// hébergeant la JVM
+		Iterable<Path> roots = FileSystems.getDefault().getRootDirectories();
+
+		// Maintenant, il ne nous reste plus qu'à parcourir
+		for (Path chemin : roots) {
+			System.out.println(chemin);
+			// Pour lister un répertoire, il faut utiliser l'objet DirectoryStream
+			// L'objet Files permet de créer ce type d'objet afin de pouvoir l'utiliser
+			try (DirectoryStream<Path> listing = Files.newDirectoryStream(chemin)) {
+
+				int i = 0;
+				for (Path nom : listing) {
+					System.out.print("\t\t" + ((Files.isDirectory(nom)) ? nom + "/" : nom));
+					i++;
+					if (i % 4 == 0)
+						System.out.println("\n");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
